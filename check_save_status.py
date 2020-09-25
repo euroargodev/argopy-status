5289ce555b2d37e3761639d6795e6c00dab95ee8
Guillaume Maze <gmaze@ifremer.fr>
2020-09-25 20:34:11 +0200

    Update check_save_status.py

--- check_save_status.py
+++ check_save_status.py
@@ -1,26 +1,28 @@
 import os
 import json
-from argopy.utilities import isAPIconnected
+from argopy.utilities import isAPIconnected, list_available_data_src
+
 
 def save_api_status(out_dir: str = '.'):
     colors = {'up': 'green', 'down': 'red'}
     flist = []
-    for api in ['erddap', 'argovis']:
-        label = "API status: %s" % api
-        status = 'down'
-        if isAPIconnected(src=api, data=1):
-            status = 'up'
-        color = colors[status]
-        message = status
-        data = {}
-        data['schemaVersion'] = 1
-        data['label'] = label
-        data['message'] = message
-        data['color'] = color
-        outfile = os.path.join(out_dir, 'argopy_api_status_%s.json' % api)
-        with open(outfile, 'w') as f:
-            json.dump(data, f)
-            flist.append(outfile)
+    for api, mod in list_available_data_src().items():
+        if hasattr(mod, 'api_server_check'):
+            label = "API status: %s" % api
+            status = 'down'
+            if isAPIconnected(src=api, data=1):
+                status = 'up'
+            color = colors[status]
+            message = status
+            data = {}
+            data['schemaVersion'] = 1
+            data['label'] = label
+            data['message'] = message
+            data['color'] = color
+            outfile = os.path.join(out_dir, 'argopy_api_status_%s.json' % api)
+            with open(outfile, 'w') as f:
+                json.dump(data, f)
+                flist.append(outfile)
     return flist
 
 if __name__ == '__main__':
